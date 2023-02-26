5ec16af7cac40b6d503c40fd7f0218b59b1ccc48
sydyk120276 <akbar@github.com>
2023-02-26 16:09:18 +0600

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,59 +1,22 @@
-// const deadline = "June 1 2023 00:00:00";
 
-// function getTimeRemaining(endtime) {
-//   var t = Date.parse(endtime) - Date.parse(new Date());
-//   var seconds = Math.floor((t / 1000) % 60);
-//   var minutes = Math.floor((t / 1000 / 60) % 60);
-//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
-//   return {
-//     total: t,
-//     days: days,
-//     hours: hours,
-//     minutes: minutes,
-//     seconds: seconds,
-//   };
-// }
-
-// function initializeClock(id, endtime) {
-//   const date = document.querySelector(id);
-//   const timeinterval = setInterval(function () {
-//     var t = getTimeRemaining(endtime)
-//     date.innerHTML =
-//       t.days +
-//       " дней " +
-//       t.hours +
-//       " часов " +
-//       t.minutes +
-//       " минут " +
-//       t.seconds +
-//       " секунд";
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }, 1000);
-// }
-
-// initializeClock(".content__time-сountdown", deadline);
-
-// валидация
+//валидация
 
 
-// let validator = new JustValidate('#form')
+let validator = new JustValidate('#form')
 
-// validator.addField('#email', [
-//   {
-//     rule: 'customRegexp',
-//     value: /^[A-Za-z0-9][A-Za-z0-9\.\-_]*[A-Za-z0-9]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\.)+[A-Za-z]*$/gi,
-//     errorMessage: 'Ошибка'
-//   },
-//   {
-//     rule: 'required',
-//     errorMessage: 'Введите правильно'
-//   },
-//   {
-//     rule: 'email',
-//     errorMessage: 'Ошибка введите правильный электронную почту'
-//   },
-// ])
+validator.addField('#email', [
+  {
+    rule: 'customRegexp',
+    value: /^[A-Za-z0-9][A-Za-z0-9\.\-_]*[A-Za-z0-9]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\.)+[A-Za-z]*$/gi,
+    errorMessage: 'Ошибка'
+  },
+  {
+    rule: 'required',
+    errorMessage: 'Введите правильно'
+  },
+  {
+    rule: 'email',
+    errorMessage: 'Введите правильно электронную почту'
+  },
+])
 
